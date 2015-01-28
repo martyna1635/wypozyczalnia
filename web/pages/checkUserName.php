eeb3ff48b90f1d29421813fefbec367fc49f2d3d
Anna Kawecka <kaweckanna@gmail.com>
2015-01-28 16:29:09 +0100

    Sprawdzanie czy login istnieje

--- web/pages/checkUserName.php
+++ web/pages/checkUserName.php
@@ -1,4 +1,12 @@
 <?PHP
-$data = ['isFree' => 0];
+require 'mysql/User.php';
+
+
 header('Content-Type: application/json');
+
+
+$user = new User; 
+// Zwraca 0 lub 1 jeśli jest wolne
+$data = ['isFree' => $user->loginExist($_GET['login'])?0:1];
+
 echo json_encode($data);
