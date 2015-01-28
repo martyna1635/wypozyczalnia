b6438bf3546af5d585fe4b53f92b4a9ddfb5f2ba
Anna Kawecka <kaweckanna@gmail.com>
2015-01-28 17:10:49 +0100

    Dodano stronę sprawdzającą poprawnośc logowania

--- web/pages/logincheck.php
+++ web/pages/logincheck.php
@@ -1,4 +1,26 @@
 <?PHP
-$data = ['success' => 0];
+// Umportowanie klasy user
+session_start();
+require 'mysql/User.php';
+// Ustawianie nagłówków dla ajax (json)
 header('Content-Type: application/json');
+
+//Tworzenie klasy user
+$user = new User();
+// Sprawdzanie czy użytkownik podane dane są prawidłowe
+$success = $user->checkAuth($_POST['username'], $_POST['password']);
+
+$data = [
+		'success' => $success?1:0
+		];
+
+
 echo json_encode($data);
+
+// Nie ustawiaj sesji gdy dane są niepoprawne
+if (!$success)
+	exit();
+
+//Ustawianie danych sesji
+$_SESSION["isLogged"] = "1";
+$_SESSION["username"] = $_POST['username'];
