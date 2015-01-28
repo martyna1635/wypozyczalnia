6b2e2bbc6744086398961d674063a16f6cf5a594
Anna Kawecka <kaweckanna@gmail.com>
2015-01-28 23:40:51 +0100

    Obsluga dodawania filmow

---
+++ web/pages/movie/add.php
@@ -0,0 +1,17 @@
+<?php
+//import bilbioteki movie
+require '../mysql/Movie.php';
+
+header('Content-Type: application/json');
+
+
+$movie = new Movie(); 
+
+
+// Zwraca 0 lub 1 jeśli pomyslnie utworzono
+$data = [
+	'created' => ($movie->create($_POST['movie_title'], $_POST['movie_description'], $_POST['movie_price'], $_POST['move_director']))?0:1
+	];
+
+echo json_encode($data);
+
