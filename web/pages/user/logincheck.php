8df51381194c3c740e08f49c0c5aded603a9e8ee
Anna Kawecka <kaweckanna@gmail.com>
2015-01-28 22:50:06 +0100

    Dodano obsługę rejestracji

--- web/pages/user/logincheck.php
+++ web/pages/user/logincheck.php
@@ -1,7 +1,7 @@
 <?PHP
 // Umportowanie klasy user
 session_start();
-require 'mysql/User.php';
+require '../mysql/User.php';
 // Ustawianie nagłówków dla ajax (json)
 header('Content-Type: application/json');
 
