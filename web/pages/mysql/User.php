1b7b362710feeafb5aa230f49ee9ba9426d14a35
Natalia Gola <natalia.gola93@gmail.com>
2015-01-28 15:50:09 +0100

    Klasa obslugujaca dodwanie nowego uzytkownika, logowanie, sprawdzanie czy login nie jest juz zajety

---
+++ web/pages/mysql/User.php
@@ -0,0 +1,41 @@
+<?php
+require_once('Database.php');
+class User {
+	// zmienna przetrzymujaca obiekt bazy danych
+	private $db; 
+	function __construct()
+	{
+		$this->db = new Database();
+	}
+
+	//funkcja tworząca nowego użytkownika
+	function create($name, $password)
+	{
+		// Zwaraca true w przypadku pomyslnego utworzenia użytkownika
+		if ($this->db->query("INSERT INTO `users` (`id`, `username`, `password`) VALUES (NULL, '$name', '$password')"))
+			return true; else
+			return false;
+	}
+	//funkcja sprawdzająca czy login nie jest juz zajęty
+	function loginExist($name){
+		$result = $this->db->query("SELECT * FROM `users` WHERE `username` = '$name'");
+		if ($result->num_rows == 0)
+			return false; else
+			return true;
+	}
+
+	//sprawdzanie czy login i hasło sa poprawne
+	function checkAuth($name, $password){
+		$result = $this->db->query("SELECT * FROM `users` WHERE `username` = '$name' AND `password` = '$password'");
+		if ($result->num_rows == 0)
+			return false; else
+			return true;
+	}
+	function getUsers()
+	{
+		$result = $this->db->query("SELECT * FROM `users`");
+		
+		return $result->theResult;
+	}
+}
+
